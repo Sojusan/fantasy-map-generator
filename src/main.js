83e5c25517ebbf961ae99b2aa0e755c1b5a4ec18
Rafał Grzegorczyk <grzegorczyk1rafal@gmail.com>
2021-05-06 00:34:31 +0200

    basic island generation added

--- src/main.js
+++ src/main.js
@@ -1,240 +1,19 @@
-// var svg = d3.selectAll("svg").filter(function (d, i) { return i === 0}),
-//     width = +svg.attr("width"),
-//     height = +svg.attr("height"),
-//     sites = d3.range(1000).map(function(d) {
-//       return [Math.random() * width, Math.random() * height];}),
-//     voronoi = d3.voronoi().extent([[0, 0],[width, height]]),
-//     diagram = voronoi(sites),
-//     polygons = diagram.polygons(),
-//     color = d3.scaleSequential(d3.interpolateSpectral);
-// polygons.map(function(i, d) {
-//   svg.append("path").attr("d", "M" + i.join("L") + "Z").attr("fill", color(d/1000));
-// });
+// on click buttons functions added
+d3.select('#delaunay-voronoi-diagram-showcase-button').on('click', function () {showHide("#delaunay-voronoi-diagram-showcase-div")});
+d3.select('#basic-island-generator-button').on('click', function () {showHide("#basic-island-generator-div")});
 
-// function relax() {
-//   // relaxation itself
-//   iteration.value = +iteration.value + 1;
-//   svg.selectAll("path").remove();
-//   sites = voronoi(sites).polygons().map(d3.polygonCentroid);
-//   diagram = voronoi(sites);
-//   polygons = diagram.polygons();
+function showHide(elementId) {
+  let x = d3.select(elementId);
 
-//   // push neighbors indexes to each polygons element
-//   polygons.map(function(i, d) {
-//     i.index = d; // index of this element
-//     var neighbors = [];
-//     diagram.cells[d].halfedges.forEach(function(e) {
-//       var edge = diagram.edges[e], ea;
-//       if (edge.left && edge.right) {
-//         ea = edge.left.index;
-//         if (ea === d) {
-//           ea = edge.right.index;
-//         }
-//         neighbors.push(ea);
-//       }
-//     })
-//     i.neighbors = neighbors;
-//     svg.append("path").attr("d", "M" + i.join("L") + "Z").attr("fill", color(d/1000));
-//   });
-//   // show 1st array element in console
-//   console.log(polygons[10]);
-// }
+  if (x.style('display') === 'none') {
+    x.style('display', 'block');
+  } else {
+    x.style('display', 'none');
+  }
+};
 
-// console.table(diagram.polygons()[0]);
-// console.table(diagram.cells[0]);
-// console.table(diagram.edges[diagram.cells[0].halfedges[0]]);
-
-// generate();
-
-// function generate() {
-// 	d3.select(".mapCells").remove();
-//   var svg = d3.selectAll("svg").filter(function (d, i) { return i === 1}),
-//       mapCells = svg.append("g").attr("class", "mapCells")
-//         .on("touchmove mousemove", (event) => {
-//           // draw circle based on options on mousemove
-//           var point = d3.pointer(event),
-//           nearest = diagram.find(point[0], point[1]).index,
-//           radius = (radiusInput.valueAsNumber)*50;
-//           console.log(point);
-//           $("#cell").text(nearest);
-//           $("#high").text((polygons[nearest].high).toFixed(2));
-//           svg.select(".radius").remove();
-//           svg.append("circle")
-//             .attr("r", radius)
-//             .attr("cx", point[0])
-//             .attr("cy", point[1])
-//             .attr("class", "radius");
-//         })
-//         .on("click", clicked),
-//       width = +svg.attr("width"),
-//       height = +svg.attr("height"),
-//       sites = d3.range(sizeInput.valueAsNumber).map(function(d) {
-//         return [Math.random() * width,
-//           Math.random() * height];}),
-//       voronoi = d3.voronoi().extent([[0, 0],[width, height]]),
-//       sites = voronoi(sites).polygons().map(d3.polygonCentroid),
-//       diagram = voronoi(sites),
-//       polygons = diagram.polygons(),
-//       color = d3.scaleSequential(d3.interpolateSpectral),
-//       queue = [];
-
-//   detectNeighbors();
-
-//   function detectNeighbors() {
-//     // push neighbors indexes to each polygons element
-//     polygons.map(function(i, d) {
-//       i.index = d; // index of this element
-//       i.high = 0;
-//       var neighbors = [];
-//       diagram.cells[d].halfedges.forEach(function(e) {
-//         var edge = diagram.edges[e], ea;
-//         if (edge.left && edge.right) {
-//           ea = edge.left.index;
-//           if (ea === d) {
-//             ea = edge.right.index;
-//           }
-//           neighbors.push(ea);
-//         }
-//       })
-//       i.neighbors = neighbors;
-//       mapCells.append("path")
-//         .attr("d", "M" + i.join("L") + "Z")
-//         .attr("id", d)
-//         .attr("class", "mapCell")
-//         .attr("fill", color(1-i.high));
-//     });
-//   }
-
-//   function add(start, type) {
-//     // get options
-//     var high = highInput.valueAsNumber,
-//         radius = radiusInput.valueAsNumber,
-//         sharpness = sharpnessInput.valueAsNumber,
-//         queue = []; // new queue
-//     polygons[start].high += high;
-//     polygons[start].used = 1;
-//     queue.push(start);
-//     for (i = 0; i < queue.length && high > 0.01; i++) {
-//       if (type == "island") {
-//       	 high = polygons[queue[i]].high * radius;
-//       } else {
-//       	high = high * radius;
-//       }
-//       polygons[queue[i]].neighbors.forEach(function(e) {
-//         if (!polygons[e].used) {
-//           var mod = Math.random() * sharpness + 1.1-sharpness;
-//           if (sharpness == 0) {mod = 1;}
-//           polygons[e].high += high * mod;
-//           if (polygons[e].high > 1) {polygons[e].high = 1;}
-//           polygons[e].used = 1;
-//           queue.push(e);
-//         }
-//       });
-//     }
-//     // re-color the polygons based on new highs
-//     polygons.map(function(i) {
-//       $("#" + i.index).attr("fill", color(1-i.high));
-//       i.used = undefined; // remove used attribute
-//     });
-//   }
-
-// 	function clicked(e) {
-//     // draw circle based on options on mousemove
-//     var point = d3.pointer(this),
-//         nearest = diagram.find(point[0], point[1]).index;
-// 		mapCells.append("circle")
-//       .attr("r", 3)
-//       .attr("cx", point[0])
-//       .attr("cy", point[1])
-//       .attr("fill", color(1 - highInput.valueAsNumber))
-//       .attr("class", "circle");
-//     if ($(".circle").length == 1) {
-// 			add(nearest, "island");
-// 			// change options to defaults for hills
-// 			highInput.value = 0.2;
-//       highOutput.value = 0.2;
-//       radiusInput.value = 0.99;
-//       radiusOutput.value = 0.99;
-//     } else {
-//     	add(nearest, "hill");
-//       // let's make high random for hills
-//       var height = Math.random() * 0.4 + 0.1;
-//       highInput.value = height;
-//       highInput.value = height;
-//     }
-//   }
-// }
-
-
-
-
-
-
-
-
-
-
-
-// var svg = d3.select("svg"),
-//   width = +svg.attr("width"),
-//   height = +svg.attr("height"),
-//   sites = d3.range(1000).map(function(d) {
-//     return [Math.random() * width, Math.random() * height];
-//   }),
-//   voronoi = d3.voronoi().extent([[0, 0],[width, height]]),
-//   diagram = voronoi(sites), polygons = diagram.polygons(),
-//   // Add spectral color range[0,1] using d3-scale-chromatic
-//  color = d3.scaleSequential(d3.interpolateSpectral);
-
-
-
-// // Draw the colored polygons
-// polygons.map(function(i, d) {
-//   svg.append("path")
-// .attr("d", "M" + i.join("L") + "Z")
-// .attr("fill", color(d/1000));
-// });
-
-
-// console.log(polygons);
-// Adding relax function
-// function relax() {
-//   // relaxation itself
-//   iteration.value = +iteration.value + 1;
-//   svg.selectAll("path").remove();
-//   sites = voronoi(sites).polygons().map(d3.polygonCentroid);
-//   diagram = voronoi(sites);
-//   polygons = diagram.polygons();
-
-//   // push neighbors indexes to each polygons element
-//   polygons.map(function(i, d) {
-//     i.index = d; // index of this element
-//     var neighbors = [];
-//     diagram.cells[d].halfedges.forEach(function(e) {
-//       var edge = diagram.edges[e], ea;
-//       if (edge.left && edge.right) {
-//         ea = edge.left.index;
-//         if (ea === d) {
-//           ea = edge.right.index;
-//         }
-//         neighbors.push(ea);
-//       }
-//   })
-//   i.neighbors = neighbors;
-//   svg.append("path")
-//     .attr("d", "M" + i.join("L") + "Z")
-//     .attr("fill", color(d/1000));
-//   });
-//   // show 1st array element in console
-//   console.log(polygons[0]);
-// }
-
-
-
-
-
-
-let svg = d3.select('svg');
+// 1. Delaunay and Voronoi diagrams
+let svg = d3.select('#delaunay-voronoi-diagram-showcase-svg');
 let width = +svg.attr('width');
 let height = +svg.attr('height');
 let sites = d3.range(1000).map(function() {
@@ -268,7 +47,7 @@ function voronoi_vertex() {
   for (let i = 0; i < sites.length; i++) {
 
     let polygon = voronoi.cellPolygon(i);
-    polygon.forEach((item, index) => {
+    polygon.forEach((item) => {
       // console.log(item)
       let is_unique = true;
       for (let j = 0; j < vertex_coords.length; j++) {
@@ -289,12 +68,12 @@ function voronoi_vertex() {
   return vertex_coords;
 };
 
-console.log( sites[0] );
-console.log( d3.polygonCentroid(voronoi.cellPolygon(0)) );
-console.log( voronoi.cellPolygon(0) );
-console.log( voronoi );
-console.log( circumcenters_coords() );
-console.log( voronoi_vertex() );
+// console.log( sites[0] );
+// console.log( d3.polygonCentroid(voronoi.cellPolygon(0)) );
+// console.log( voronoi.cellPolygon(0) );
+// console.log( voronoi );
+// console.log( circumcenters_coords() );
+// console.log( voronoi_vertex() );
 
 draw_all();
 
@@ -320,6 +99,7 @@ function relax() {
   // update delaunay and voronoi
   delaunay = d3.Delaunay.from( sites, d => d.x, d => d.y );
   voronoi = delaunay.voronoi([0, 0, width, height]);
+
   // draw updated diagrams
   draw_all();
 }
@@ -336,7 +116,7 @@ function draw_all() {
     .enter()
     .append('path')
       .attr('d', d => d)
-      .style('fill', (_,i) => 'grey')
+      .style('fill', (_) => 'grey')
       .style('opacity', 1)
       .style('stroke', 'white')
       .style('stroke-opacity', 1);
@@ -349,7 +129,7 @@ function draw_all() {
     .enter()
     .append('path')
       .attr('d', d => d)
-      .style('fill', (_,i) => 'rgba(255, 255, 255, 0)')
+      .style('fill', (_) => 'rgba(255, 255, 255, 0)')
       .style('opacity', 1)
       .style('stroke', 'black')
       .style('stroke-opacity', 1);
@@ -374,4 +154,138 @@ function draw_all() {
     .attr('cy', d => d.y)
     .attr('r', 3)
     .style('fill', 'blue');
+};
+
+
+
+
+// 2. Basic island generator
+d3.select('#basic-island-generator-generate-button').on('click', basicIslandGenerator);
+
+function basicIslandGenerator() {
+  d3.select('#basic-island-generator-div').select('.mapCells').remove();
+
+  let svg = d3.select('#basic-island-generator-svg');
+  let mapCells = svg.append('g').attr('class', 'mapCells')
+    .on('click', clicked)
+    .on("touchmove mousemove", moved);
+  let width = +svg.attr('width');
+  let height = +svg.attr('height');
+  let sites = d3.range(basicIslandGeneratorSizeInput.valueAsNumber).map(function() {
+    return {
+      x: Math.random() * width,
+      y: Math.random() * height
+    };
+  });
+  let delaunay = d3.Delaunay.from( sites, d => d.x, d => d.y );
+  let voronoi = delaunay.voronoi([0, 0, width, height]);
+  let polygons = Array.from(voronoi.cellPolygons());
+  let color = d3.scaleSequential(d3.interpolateSpectral);
+  let queue = [];
+
+  d3.select('#basic-island-generator-toggle-circles-button').on('click', toggleCircles);
+
+  function toggleCircles() {
+    if (mapCells.selectAll('.circle').style('opacity') == 0) {
+      mapCells.selectAll('.circle').style('opacity', 1);
+    } else {
+      mapCells.selectAll('.circle').style('opacity', 0);
+    }
+  };
+
+  for (let value of polygons) {
+    value.height = 0;
+    mapCells.append('path')
+      .attr('d', 'M' + value.join('L') + 'Z')
+      .attr('id', 'c' + value.index)
+      .attr('class', 'mapCell')
+      .attr('fill', color(1 - value.height));
+  };
+
+  function add(start, type) {
+    // get options from inputs
+    let height = basicIslandGeneratorHeightInput.valueAsNumber;
+    let radius = basicIslandGeneratorRadiusInput.valueAsNumber;
+    let sharpness = basicIslandGeneratorSharpnessInput.valueAsNumber;
+    queue = []; // new queue
+    polygons[start].height += height;
+    polygons[start].used = 1;
+    queue.push(start);
+
+    for (let i = 0; i < queue.length && height > 0.01; i++) {
+      if (type == 'island') {
+        height = polygons[queue[i]].height * radius;
+      } else {
+        height *= radius;
+      }
+
+      for (let neighbor of voronoi.neighbors(queue[i])) {
+        if (!polygons[neighbor].used) {
+          // calculate the modifier
+          let mod = Math.random() * sharpness + 1.1 - sharpness;
+
+          // if sharpness is 0 modifier should be ignored (=1)
+          if (sharpness == 0) {mod = 1;}
+
+          polygons[neighbor].height += height * mod;
+
+          // max height is 1
+          if (polygons[neighbor].height > 1) {
+            polygons[neighbor].height = 1;
+          };
+
+          polygons[neighbor].used = 1;
+          queue.push(neighbor);
+        };
+      };
+    };
+    // re-color the polygons based on new heights
+    for (let value of polygons) {
+      mapCells.select('#c' + value.index).attr('fill', color(1 - value.height));
+      value.used = undefined; // remove used attribute
+    };
+  };
+
+  function clicked(e) {
+    // draw circle based on options on mousemove
+    let point = d3.pointer(e);
+    let nearest = voronoi.delaunay.find(point[0], point[1]);
+    mapCells.append("circle")
+      .attr("r", 3)
+      .attr("cx", point[0])
+      .attr("cy", point[1])
+      .attr("fill", color(1 - basicIslandGeneratorHeightInput.valueAsNumber))
+      .attr('opacity', 0)
+      .attr("class", "circle");
+
+    if (mapCells.selectAll('.circle').size() == 1) {
+      add(nearest, "island");
+			// change options to defaults for hills
+			basicIslandGeneratorHeightInput.value = 0.2;
+      basicIslandGeneratorHeightOutput.value = 0.2;
+      basicIslandGeneratorRadiusInput.value = 0.99;
+      basicIslandGeneratorRadiusOutput.value = 0.99;
+    } else {
+      add(nearest, "hill");
+      // let's make high random for hills
+      let height = Math.random() * 0.4 + 0.1;
+      basicIslandGeneratorHeightInput.value = height.toFixed(2);
+      basicIslandGeneratorHeightOutput.value = height.toFixed(2);
+    }
+  };
+
+  function moved(e) {
+    // draw circle based on options on mousemove
+    let point = d3.pointer(e);
+    let nearest = voronoi.delaunay.find(point[0], point[1]);
+    let radius = (basicIslandGeneratorRadiusInput.valueAsNumber - 0.94) * 500;
+    d3.select('#basic-island-generator-cell').text(nearest);
+    d3.select('#basic-island-generator-height').text(polygons[nearest].height.toFixed(2));
+    svg.select('.radius').remove();
+    svg.append('circle')
+      .attr('r', radius)
+      .attr('cx', point[0])
+      .attr('cy', point[1])
+      .attr('class', 'radius')
+  };
 };
